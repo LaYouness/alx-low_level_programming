8902c0b1055440c43140eb1d3932a19699da2f2e
Youness Laarare <youness.laarare@gmail.com>
2023-03-21 13:54:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+	_putchar(i);
+	i++;
+	}
+	_putchar('\n');
+}
