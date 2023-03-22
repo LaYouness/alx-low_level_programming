52dae218f13c2e59b6f8da7567acce61436049c5
Youness Laarare <youness.laarare@gmail.com>
2023-03-21 18:38:18 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,15 +15,24 @@ for (j = 0; j <= 9; j++)
 m = i * j;
 m2 = m % 10;
 m1 = m / 10;
-_putchar(m1 + 48);
-_putchar(m2 + 48);
-if (j == 9)
+if (m == 0)
 {
-continue;
+_putchar(m2 + 48);
 }
+else if (m1 == 0)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+_putchar(m2 + 48);
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(m1 + 48);
+_putchar(m2 + 48);
+}
 }
 _putchar('\n');
 }
