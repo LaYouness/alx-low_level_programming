744a949974847318381fb8a346abe77112f8acaa
Youness Laarare <youness.laarare@gmail.com>
2023-04-03 09:27:06 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strchr - find a char in a string
+ *@s: string
+ *@c: char we are searching for
+ *Return: Returns the str when c found
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return ((char *) s);
+}
+s++;
+}
+return ('\0');
+}
