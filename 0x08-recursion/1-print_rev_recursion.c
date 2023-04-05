83921eac0906db2d47ccfd06ab78a513080a5ee3
Youness Laarare <youness.laarare@gmail.com>
2023-04-05 11:08:34 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-s++;
-_print_rev_recursion(s);
-_putchar(*s);
+_putchar('\n');
+return;
 }
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
 }
