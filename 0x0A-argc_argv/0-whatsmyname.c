9081c10967a48ce4fc725e95cd61589deb26542b
Youness Laarare <youness.laarare@gmail.com>
2023-04-10 16:57:28 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: num arg
+ * @argv: args
+ * Return always 0
+ */
+int main(int argc,char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
