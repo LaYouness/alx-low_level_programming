337b3491df38a868bd6a8b3f80bcafba38f672f0
Youness Laarare <youness.laarare@gmail.com>
2023-04-10 09:34:43 -0700

    libmy.a

---
+++ 0x09-static_libraries/_atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _atoi - entry
+ * @s: input
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	_putchar(s[0]);
+return (0);
+}
---
+++ 0x09-static_libraries/_strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strstr - entry
+ *@haystack: input
+ *@needle: input
+ *Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	_putchar(haystack[0]);
+	_putchar(needle[0]);
+return(needle);
+}
