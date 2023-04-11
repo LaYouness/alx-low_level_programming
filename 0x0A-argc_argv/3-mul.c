8cdb33740c0b639577fd885162ebdd7c70eedc6d
Youness Laarare <youness.laarare@gmail.com>
2023-04-10 17:38:49 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry
+ *@argc: num of args
+ *@argv: args
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int s;
+
+if (argc < 3)
+{
+printf("Error");
+return (1);
+}
+else
+{
+s = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", s);
+return (0);
+}
+}
